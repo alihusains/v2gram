d0378a944ce2ec1112b0a524703517fdb2fba40a
alihusains <123808017+alihusains@users.noreply.github.com>
2026-01-26 10:42:38 +0400

    Update channel_assets.php

--- modules/channel_assets.php
+++ modules/channel_assets.php
@@ -12,7 +12,7 @@ foreach ($Types as $channel => $data_array){
     file_put_contents("modules/channels/" . $channel . ".jpg", file_get_contents($image_match[1]));
     $channel_array[$channel]['types'] = $data_array;
     $channel_array[$channel]['title'] = $title_match[1];
-    $channel_array[$channel]['logo'] = "https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/modules/channels/" . $channel . ".jpg";
+    $channel_array[$channel]['logo'] = "https://raw.githubusercontent.com/alihusain/v2gram/main/modules/channels/" . $channel . ".jpg";
 }
 
 file_put_contents("modules/channels/channels_assets.json", json_encode($channel_array , JSON_PRETTY_PRINT));
